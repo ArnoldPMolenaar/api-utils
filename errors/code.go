9b4f46887b664676f971fa934de908d2bf975b01
Arnold <arnold.molenaar@segesia.nl>
2024-12-16 08:00:07 +0100

    ✨ feat: Added error codes from the api-auth that are for general use
    Changelog: feature

--- errors/code.go
+++ errors/code.go
@@ -2,8 +2,16 @@ package errors
 
 // Define error codes as constants.
 const (
-	NotFound            = "notFound"
-	Unauthorized        = "unauthorized"
-	InternalServerError = "internalServerError"
+	NotFound             = "notFound"
+	Unauthorized         = "unauthorized"
+	InternalServerError  = "internalServerError"
+	BodyParse            = "bodyParse"
+	Validator            = "validator"
+	QueryError           = "queryError"
+	CacheError           = "cacheError"
+	Forbidden            = "forbidden"
+	MissingRequiredParam = "missingRequiredParam"
+	InvalidParam         = "invalidParam"
+	OutOfSync            = "outOfSync"
 	// Add more error codes as needed.
 )
